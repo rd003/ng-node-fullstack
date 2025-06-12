5b7d936b7953b59752556cd0eebe411548db4132
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-06-12 20:40:16 +0530

    feat: people form validation

--- client/src/app/person/person.component.ts
+++ client/src/app/person/person.component.ts
@@ -1,12 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-person',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: 'person.component.html',
     styles: [``]
 })
 export class PersonComponent {
+    fb = inject(FormBuilder);
 
+    get f() {
+        return this.personForm.controls;
+    }
+
+    personForm = this.fb.group({
+        id: [0],
+        firstName: ['', [Validators.required, Validators.maxLength(30)]],
+        lastName: ['', [Validators.required, Validators.maxLength(30)]]
+    });
+
+    onSubmit() {
+        console.log(this.personForm.value);
+    }
+
+    onCancel = () => this.personForm.reset();
+
+    onEdit(person: any) {
+        this.personForm.patchValue(person);
+    }
+
+    onDelete(person: any) {
+        if (window.confirm('Are your sure to delete?')) {
+
+        }
+    }
 }
