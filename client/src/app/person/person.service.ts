60e53f464641a304dfd012f81404d1fe2f0fcd7f
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-06-13 13:02:55 +0530

    feat: person service is created

---
+++ client/src/app/person/person.service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from "@angular/core";
+import { environment } from "../../environments/environment.development";
+import { HttpClient } from "@angular/common/http";
+import { PersonModel } from "./person.model";
+
+@Injectable({ providedIn: "root" })
+export class PersonService {
+    private readonly apiUrl = environment.BASE_URL + "/people";
+    private readonly http = inject(HttpClient);
+
+    getAll() {
+        return this.http.get<Array<PersonModel>>(this.apiUrl);
+    }
+
+    getOne(id: number) {
+        return this.http.get<PersonModel>(`${this.apiUrl}/${id}`);
+    }
+
+    delete(id: number) {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+
+    update(person: PersonModel) {
+        return this.http.put<void>(`${this.apiUrl}/${person.id}`, person);
+    }
+
+    post(person: PersonModel) {
+        return this.http.post<PersonModel>(this.apiUrl, person);
+    }
+}
